1564cde7d644f4b16d626ca4a3693774cac99b99
anna.tender@bk.ru <anna.tender@bk.ru>
2025-07-24 23:04:24 +0400

    modified commit

--- dom/jest.config.js
+++ dom/jest.config.js
@@ -1,15 +1,40 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  resetMocks: true,
-  clearMocks: true,
-  moduleNameMapper: {
-    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js'
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  modulePathIgnorePatterns: [
-    '<rootDir>/anim/',
-    '<rootDir>/dnd/',
-    '<rootDir>/env/',
-    '<rootDir>/env2/'
-  ]
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['js'],
+    transform: {},
+    resetMocks: true,
+    clearMocks: true,
+
+    moduleNameMapper: {
+        '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|scss)$': 'identity-obj-proxy'
+    },
+
+    setupFilesAfterEnv: [
+        '@testing-library/jest-dom'
+    ],
+
+    modulePathIgnorePatterns: [
+        '<rootDir>/anim/',
+        '<rootDir>/dnd/',
+        '<rootDir>/env/',
+        '<rootDir>/env2/',
+        '<rootDir>/http/backend',
+        '<rootDir>/rxjs/backend',
+        '<rootDir>/workers/webpack-web-worker',
+        '<rootDir>/workers/webpack-service-worker'
+    ],
+
+    haste: {
+        throwOnModuleCollision: false
+    },
+
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/'
+    ],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/__mocks__/'
+    ]
 };
